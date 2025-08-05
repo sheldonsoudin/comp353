d6548f8374cfbedee01880200cfbee95f5c888c7
Sheldon Soudin <sheldon.soudin@gmail.com>
2025-08-05 11:32:25 -0400

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,10 @@
+host="localhost"  # e.g., "127.0.0.1" if not local
+user="root"  # e.g., "root"
+password=""  # e.g., "your_secure_password"
+database=""  # e.g., "volleyball_club"
+
+# Add your queries here and please label them question wise
+queries = [
+    "", # Question number 1
+    "", # Question number 2
+]
